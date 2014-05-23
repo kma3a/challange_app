6f53daec98850fd541a4215c4cdbca8d40a027ad
Kelsey <kma3a@virginia.edu>
2014-05-23 14:04:19 -0500

    changed the rspec tests for tests and changed challenges to tests

--- spec/models/solution_spec.rb
+++ spec/models/solution_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe Solution do
 	it {should belong_to(:user)}
-	it {should belong_to(:challenge)}
+	it {should belong_to(:test)}
 	it {should have_many(:votes)}
 	it {should have_many(:comments)}
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,9 +1,9 @@
 require 'spec_helper'
 
 describe User do
-	it {should have_many(:challenges)}
+	it {should have_many(:tests)}
 	it {should have_many(:solutions)}
-	it {should have_many(:solved_challenges).through(:solutions).source(:challenges)}
+	it {should have_many(:solved_tests).through(:solutions).source(:tests)}
 	it {should have_many(:votes)}
 	it {should have_many(:comments)}
 end
