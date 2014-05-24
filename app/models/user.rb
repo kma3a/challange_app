9b98db3a3d67b502bd8d67ff844fda4143f170fa
Kelsey <kma3a@virginia.edu>
2014-05-24 12:25:57 -0500

    added username to sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,11 @@
 class User < ActiveRecord::Base
+
   # Include default devise modules. Others available are:
   has_many :chals, foreign_key: "creator_id"
   has_many :solutions
   has_many :solved_tests, through: :solutions, class_name: "Test"
   has_many :votes
   has_many :comments, foreign_key: "author_id"
-
-  before_action :authenticate_user!
 
 
   devise :database_authenticatable, :registerable,
