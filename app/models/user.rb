fb22ed3a63b0a6362c3c490d742258bf99e9d01e
Kelsey <kma3a@virginia.edu>
2014-06-04 16:41:39 -0500

    added created chall and solved chall to user#show but found error in association

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   has_many :chals, foreign_key: "creator_id"
   has_many :solutions
-  has_many :solved_tests, through: :solutions, class_name: "Test"
+  has_many :solved_tests, through: :solutions, source: "chal"
   has_many :votes
   has_many :comments, foreign_key: "author_id"
 
