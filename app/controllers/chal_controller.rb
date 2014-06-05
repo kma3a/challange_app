5c3665300a4ef3b2b9b65e1a522af02bd95021bf
Kelsey <kma3a@virginia.edu>
2014-06-05 15:16:47 -0500

    added the methods in the chal controller

--- app/controllers/chal_controller.rb
+++ app/controllers/chal_controller.rb
@@ -1,4 +1,32 @@
 class ChalController < ApplicationController
-  
+
+	def index
+
+	end
+
+	def create
+
+	end
+
+	def new
+
+	end
+
+	def edit
+
+	end
+
+	def show
+
+	end
+
+	def update
+
+	end
+
+	def destroy
+
+	end
+
 
 end
