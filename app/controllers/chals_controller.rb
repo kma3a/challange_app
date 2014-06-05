0163bc318b804ae1f3c1c8987f2ab3a2e18cfd3e
Kelsey <kma3a@virginia.edu>
2014-06-05 15:45:15 -0500

    added the views and modified routes and names

---
+++ app/controllers/chals_controller.rb
@@ -0,0 +1,32 @@
+class ChalsController < ApplicationController
+
+	def index
+
+	end
+
+	def create
+
+	end
+
+	def new
+		@chal = Chal.new
+	end
+
+	def edit
+
+	end
+
+	def show
+
+	end
+
+	def update
+
+	end
+
+	def destroy
+
+	end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
    root 'welcome#index'
 
-   resources :chal
+   resources :chals
 
 
 
